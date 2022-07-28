6a1b20a3d99b0ab8fd8fc3f989523a8caa750278
Peyton Zeller-Av <83460468+262730@users.noreply.github.com>
2022-07-28 10:19:37 -0600

    Randomize List Based On Math.Random() milliseconds

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,70 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+let id = 0;
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      newEntryName: "",
+      randomized: false,
+      entries: [],
+    };
+  },
+  computed: {
+    getList() {
+      if (this.randomized) {
+        const randomNumList = this.entries.map((e) => {
+          return Object.assign({}, e, { randomNum: Math.random() });
+        });
+        // Console log every entry's assigned random number in list
+        const sortedRandomNumList = randomNumList.sort(
+          (a, b) => a.randomNum - b.randomNum
+        );
+        console.log(sortedRandomNumList);
+        // return sorted list
+        return sortedRandomNumList;
+      }
+      // default return non-sorted list
+      return this.entries;
+    },
+  },
+  methods: {
+    addEntry() {
+      const newEntry = { id: id++, name: this.newEntryName, randomNum: null };
+      this.entries.push(newEntry);
+      // log new entry
+      console.log("New Entry: ", newEntry);
+      this.newEntryName = "";
+    },
+    removeEntry(entry) {
+      this.entries = this.entries.filter((e) => e.id !== entry.id);
+    },
+    resetList() {
+      this.entries = [];
+      this.randomized = false;
+    },
+  },
+};
 </script>
 
+<template>
+  <form @submit.prevent="addEntry">
+    <input v-model="newEntryName" />
+    <button>Add Entry</button>
+  </form>
+  <ul>
+    <li v-for="entry in getList" :key="entry.id">
+      <span>{{ entry.name }}</span>
+      <button v-if="!randomized" @click="removeEntry(entry)">X</button>
+    </li>
+  </ul>
+  <button v-if="!randomized && entries.length > 1" @click="randomized = true">
+    {{ "Randomize!" }}
+  </button>
+  <button v-if="randomized" @click="resetList()">
+    {{ "Reset List" }}
+  </button>
+</template>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+/* TODO */
 </style>
